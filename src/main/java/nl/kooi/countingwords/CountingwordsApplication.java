8df8ce037e8346df5961a566a2bf80fcdd609b65
Laurens van der Kooi <lvvdkooi@hotmail.com>
2022-01-14 19:50:44 +0100

    Initial commit

---
+++ src/main/java/nl/kooi/countingwords/CountingwordsApplication.java
@@ -0,0 +1,13 @@
+package nl.kooi.countingwords;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountingwordsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountingwordsApplication.class, args);
+	}
+
+}
